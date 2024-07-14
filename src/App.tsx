c93b77b4dc94fc29ffef5bbcd7e0f49677dd4b4c
gerson aguiar <gersoncafilho@gmail.com>
2024-07-14 11:59:46 -0300

    construindo UI - aula 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,57 @@
+import { ArrowRight, Calendar, MapPin } from "lucide-react"
+
 function App() {
 
   return (
 
-    <h1 className="text-3xl font-bold underline bg-cyan-400">
-      travel.com
-    </h1>
+    <div className="h-screen flex items-center justify-center bg-quadrados bg-no-repeat bg-center">
 
+      <div className="max-w-3xl w-full px-6 text-center space-y-10">
+
+
+        {/* LOGO E TITULO */}
+        <div className="flex flex-col items-center gap-3">
+          <img src="/logo-travel.svg" alt="travel logo" />
+          <p className="text-zinc-300 text-lg">
+            Convide seus amigos e planeje sua próxima viagem!
+          </p>
+        </div>
+
+        {/* INPUT */}
+        <div>
+
+          <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+
+            <div className="flex items-center gap-2 flex-1">
+              <MapPin className="size-5 text-zinc-400" />
+              <input type="text" placeholder="Para onde voce vai?"
+                className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+              />
+            </div>
+
+            <div className="flex items-center gap-2">
+
+              <Calendar className="size-5 text-zinc-400" />
+              <input type="text" placeholder="Quando?"
+
+                className="bg-transparent text-lg placeholder-zinc-400  w-40 outline-none flex-1"
+              />
+            </div>
+
+            <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2  font-medium flex items-center gap-2 hover:bg-lime-400">
+              Continuar
+              <ArrowRight className="size-5" />
+            </button>
+          </div>
+        </div>
+
+        {/* TEXTO PEQUENO APOS INPUT */}
+        <p className="text-sm text-zinc-500">
+          Ao planejar sua viagem pela plann.er você automaticamente concorda <br />
+          com nossos <a href="#" className="text-zinc-300 underline"> termos de uso</a> e <a href="#" className="text-zinc-300 underline">políticas de privacidade</a>.
+        </p>
+      </div>
+    </div>
   )
 }
 
